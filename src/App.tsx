31ca939d8618b5a410ae6ce86baa0316ad4228a6
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-06-19 11:48:45 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface MyTask {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+
+  const [newTask, setNewTask] = useState<string>('')
+  const [tasks, setTasks] = useState<MyTask[]>([])
+
+  const handleSubmit = (event: FormElement) => {
+    event.preventDefault();
+    addTask(newTask);
+    setNewTask('');
+  }
+
+  const addTask = (name: string) => {
+    const newTasks: MyTask[] = [...tasks, { name, done: false }]
+    setTasks(newTasks)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+        <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <input type="text"
+              onChange={event => setNewTask(event.target.value)}
+              value={newTask}
+              className="form-control"
+              autoFocus
+              />
+            <button className="btn btn-outline-success btn-block mt-3">Create task</button>
+          </form>
+        </div>
+      </div>
+      {tasks.map((task: MyTask, i: number) => (
+          <div className="card card-body mt-2" key={i}>
+            <h2 style={{textDecoration: task.done ? 'line-through' : ''}}>{task.name}</h2>
+            <p>{task.done}</p>
+          </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
