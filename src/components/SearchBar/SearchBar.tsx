04b3bfd078149853fb2e9b96899bf28d89a28474
Jesinug <jesica.nugnez@gmail.com>
2021-07-11 20:17:31 +0200

    Logic and funcionality. Working in filterCards function of SerachBar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
+
+
 export const SearchBar= () => {
+
+  const filterCards = () => {
+    console.log('pepe')
+}
+
         return (
                 <TextField
                     fullWidth
                     id="standard-basic"
                     label="Search..."
                     placeholder='Search...'
+                    onChange={filterCards}
                 />
         );
     };
